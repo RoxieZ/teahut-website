6c7901bce7ed7e342beb4aebbeb28f39bcb9e0c7
Roxie Zhao <roxiezhao.rz@gmail.com>
2018-04-12 12:49:10 -0700

    update

--- teahut.js
+++ teahut.js
@@ -1,3 +1,13 @@
 let html = document.querySelector('html');
+let send = document.querySelector('#send');
+let requestForm = document.querySelector('#editWindow');
+let requestCancel = document.querySelector('#cancelButton');
 
-html.style.fontSize = `${(window.innerWidth/36)*6.25}%`;
+html.style.fontSize = `${(window.innerWidth/36)*5}%`;
+send.addEventListener("click",()=>{
+	editWindow.style.display = "block";
+});
+
+requestCancel.addEventListener("click",()=>{
+	editWindow.style.display = "none";
+});
